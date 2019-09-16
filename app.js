acc73e426cab18adbf194248b9936c95a09d8ea7
SharonMaillet <46636087+SharonMaillet@users.noreply.github.com>
2019-09-16 19:24:38 +0200

    Refactorized code

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 //jshint esversion:6
-
 // require les packages installer avec npm
 require('dotenv').config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require('mongoose');
+const express = require("express"); //web framework for node
+const fileUpload = require('express-fileupload'); //express middleware for uploading files.
+const bodyParser = require("body-parser"); //Node.js body parsing middleware.
+const ejs = require("ejs"); //templating language that lets generate HTML markup with plain JS
+const mongoose = require('mongoose'); //MongoDB object modeling for Node.js
 const ObjectId = mongoose.Types.ObjectId;
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+const session = require('express-session'); //Create a session middleware
+const passport = require('passport'); // authentication middleware for Node.js.
+const passportLocalMongoose = require('passport-local-mongoose'); // Mongoose plugin that simplifies building username and password login with Passport.
 const path = require("path"); //core ejs module
-const multer = require("multer");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //moteur de modèle à utiliser
 
+app.use(bodyParser.json());       
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -31,38 +31,13 @@ app.use(session({ //express-session
 app.use(passport.initialize());
 app.use(passport.session()); //passport
 
-mongoose.connect("mongodb+srv://sharon:admin@cluster0-uvxzz.mongodb.net/savDB", { useNewUrlParser: true }); //connexion a la bdd savDB
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+mongoose.connect("mongodb+srv://sharon:admin@cluster0-uvxzz.mongodb.net/savDB", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }); //connexion a la bdd savDB
      
-
-// Set The Storage Engine
-var storage = multer.diskStorage({
-    destination: 'public/upload',
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
-    }
-});
-
-var upload = multer({ storage: storage });
-
-
-// Check File Type
-function checkFileType(file, cb) {
-    // Allowed ext
-    const filetypes = /jpeg|jpg|png/;
-    // Check ext
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // Check mime
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: Images Only!');
-    }
-}
-
-
-const companySchema = {  //schema de la db
+const companySchema = {  //on defini le schema de la db, cela permet de définir les types de variables et de structurer vos données
     name: String,
     phone: Number,
     phone2: Number,
@@ -78,34 +53,31 @@ const userSchema = new mongoose.Schema ({
 
 userSchema.plugin(passportLocalMongoose); //hash and salt
 
-const Company = mongoose.model("Company", companySchema);
+const Company = mongoose.model("Company", companySchema);  //permet d'insérer des données dans MongoDB en respectant le schéma précisé et d'aller faire des requêtes
 
 const User = new mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
+passport.use(User.createStrategy());     //a metter apres User car sinon User n'est pas défini
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {  //affichage de la page home
-    
     Company.find({}, function (err, foundCompanies) {
         res.render("home", {
             name: foundCompanies.name,
             companies: foundCompanies
         });
-    }).sort({ name: 1 });
+    }).sort({ name: 1 }); // Ordre Alphabetique
 });
 
 app.get("/savs", function (req, res) {  //affichage de la page home
-    
     Company.find({}, function (err, foundCompanies) {
         res.render("liste-savs", {
             name: foundCompanies.name,         
             companies: foundCompanies
-            
         });
-    }).sort({name:1}); // Ordre Alphabetique
+    }).sort({name:1}); 
 });
 
 app.get("/company", function (req, res) {  //affichage de la page de la société
@@ -119,8 +91,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     User.register({username: req.body.username}, req.body.password, function(err, user) {
         if (err) {
-            console.log(err);
-            res.redirect("admin");
+            res.redirect("register");
         } else {
             passport.authenticate("local")(req, res, function() {
                 res.redirect("admin");
@@ -143,7 +114,7 @@ app.post("/login", function (req, res) {
 
     req.login(user, function (err) {
         if (err) {
-            console.log(err);
+            res.redirect("/login");
         } else {
             passport.authenticate("local")(req, res, function () {
                 res.redirect("admin");
@@ -153,14 +124,10 @@ app.post("/login", function (req, res) {
 
 });
 
-app.get("/company/:CompName", function (req, res) {  //affichage de la page de la société
-    if (err) {
-        res.redirect('home');
-    } else {
+app.get("/company/:CompName", function (req, res, err) {  //affichage de la page de la société
     const requestedCompany = req.params.CompName;
-
+    
     Company.findOne({name: requestedCompany}, function(err, company){
-        
         res.render("company", {
             name: company.name,
             phone: company.phone,
@@ -168,16 +135,12 @@ app.get("/company/:CompName", function (req, res) {  //affichage de la page de l
             mail: company.mail,
             filename: company.filename,
             companies: company
-        });            
-
-    });         
-    }
-
- 
+        });             
+    });           
 });
 
 app.get("/admin", function (req, res) {  //affichage de la page de la admin
-   /* if(req.isAuthenticated()) { */
+    if(req.isAuthenticated()) { //si admin est authentifier
         Company.find({}, function (err, company) {
             res.render("admin", {
                 name: company.name,
@@ -188,99 +151,93 @@ app.get("/admin", function (req, res) {  //affichage de la page de la admin
                 companies: company
             });
         }).sort({ name: 1 });
-    /*} else {
+    } else {
         res.redirect("login");
-    } */
+    }
 });
 
-app.post("/admin", function (req, res) {  //affichage de la page de la admin add
+app.post("/admin", function (req, res) {  //affichage de la page de la admin add   
+    let fileExt = req.files.logo.mimetype; //pour filter seulement les images
+    let lastSlash = fileExt.lastIndexOf('/'); //pour avoir l'extention
+    let ext = fileExt.substring(lastSlash + 1);
     
-    const company = new Company({
+    let newFileName = "logo-" + req.body.name.toLowerCase() + "." + ext;
+    let clearName = newFileName.replace(/\s+/g, ''); //le nom sans espace et characteres spéciaux
+
+    const company = new Company({ //creation d'une nouvelle entrée
         name: req.body.name,
         phone: req.body.phone,
         phone2: req.body.phone2,
         mail: req.body.mail,
-        type: req.body.type
-    });
-
-    global.newItem = req.body.name;
-
-    company.save(function (err) {
+        type: req.body.type,
+        filename: clearName
+    });    
+    
+    if( (!req.files || (fileExt !== 'image/jpeg' && fileExt !== 'image/png')) ) { //si il n'y a pas de fichier ou que le fichier n'est pas une image
+        res.redirect("/admin");
+    } else {
+        let logo = req.files.logo;
+        //Use the mv() method to place the file the upload directory
+        logo.mv('public/upload/' + clearName);
+        //send response
+        company.save(function (err) {
         if (!err) {
-            res.redirect("/admin-upload");
-            console.log("Item added successfully");
-            newItem = req.body.name;
-            console.log(newItem);
+            res.redirect("/admin");
         } else {
-            console.log(err);
             res.redirect("/admin");
         }
     });
-});
-
-app.get("/admin-edit", function (req, res) {  //affichage de la page de la admin
-    Company.find({}, function (err, company) {
-        res.render("admin-edit", {
-            id: company._id,
-            name: company.name,
-            phone: company.phone,
-            phone2: company.phone2,
-            mail: company.mail,
-            type: company.type,
-            filename: company.filename,
-            companies: company
-        });
-    }).sort({ name: 1 });
+}
 });
 
 
-app.post("/admin-edit", function (req, res) {  //affichage de la page de la admin add
-
-    var id = ObjectId(req.body.id);
-
-    Company.updateOne({ _id: id }, { name:req.body.name, phone:req.body.phone}, function (err) {
-        if (err)
-            throw (err);
+app.post("/admin-edit", function (req, res) {  //edition
+  
+    if (!req.files) { //si il n'y a pas de nouveau fichier, on update toutes les autres entrées SAUF fichier
+    let id = ObjectId(req.body.id);
+    Company.updateOne({ _id: id }, { name:req.body.name, phone:req.body.phone, phone2:req.body.phone2,  mail:req.body.mail,  type:req.body.type}, function (err) {
+        if (err) {
+        throw (err);   //si il y a une erreur, on passe a la ligne suivante         
+        }
     });
-    res.redirect("/admin");
+    res.redirect("/admin");        
+    } else { // sinon on update tout
+        let fileExt = req.files.newFile.mimetype;
+        let lastSlash = fileExt.lastIndexOf('/');
+        let ext = fileExt.substring(lastSlash + 1);
+
+        if( fileExt !== 'image/jpeg' && fileExt !== 'image/png' ) {
+            res.redirect("/admin"); //si les fichiers ne sont pas des images on redirige vers admin et on stop l'update
+        } else {
+            let newFile = req.files.newFile;
+            let newFileName = "logo-" + req.body.name.toLowerCase() + "." + ext;
+            let clearName = newFileName.replace(/\s+/g, '');
+            newFile.mv('public/upload/' + clearName);
+            //on update tout
+            Company.updateOne({name: req.body.name}, { phone:req.body.phone, phone2:req.body.phone2,  mail:req.body.mail, type:req.body.type, filename: clearName}, function(err){
+                if (err) {
+                    console.log(err);
+                    res.redirect("/admin");
+                }
+            });
+            res.redirect("/admin");
+        }
+    }
 }); 
 
 
 app.post("/admin-delete", function (req, res) {
-    const deletedItem = req.body.suppr;
-    
-    Company.findByIdAndDelete(deletedItem, function (err) {
+
+   const deletedItem = req.body.suppr;
+
+   Company.findByIdAndDelete(deletedItem, function (err) {
         if (!err) {
-            console.log("Successfully deleted item");
             res.redirect("/admin");
         } else {
-            res.send("Error");
+            res.redirect("/admin");
         }
     });
 });
-
-app.post("admin-update", function (req, res) {
-
-});
-
-app.get("/admin-upload", function (req, res) {  //affichage de la page de la admin
-        res.render("admin-upload");
-});
-
-app.post("/admin-upload", upload.single("logo"), function (req, res) {  //affichage de la page de la admin add
-    
-   const company = Company({
-        name: newItem,
-        filename: req.file.fieldname
-    });
-    filename = req.file.filename
-    Company.updateOne({ name: newItem }, { $set: { filename: filename }}, function (err) {
-        if (err)
-            throw (err);          
-    }); 
-    res.redirect('/admin');
-}); 
-
 
 
  app.listen(3000, function () { //connexion au localhost
